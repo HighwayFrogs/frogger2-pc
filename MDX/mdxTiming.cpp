833ed33b3eadcf98bad21def0ee0327d96c4f093
Kneesnap <davidmenlo2@gmail.com>
2025-01-30 04:51:02 -0800

    Prevent Frogger 2 from having too high FPS values.

--- MDX/mdxTiming.cpp
+++ MDX/mdxTiming.cpp
@@ -50,16 +50,16 @@ void InitTiming(float frameSpeed)
 void UpdateTiming(void)
 {
 	float mult = (timeInfo.frameSpeed/1000.0F);
-	//Maybe waiting here will help?
-
-	unsigned long currentTicks = (timeGetTime()+timeInfo.tickModifier) - timeInfo.firstTicks;
-	while (currentTicks == timeInfo.tickCount)
-	{
+	unsigned long currentTicks;
+	
+	// Ensure at least one millisecond passes to avoid having ticks with a gameSpeed (deltaTime) of zero.
+	// I really wanted a better solution than this, but unfortunately due to using fixed point numbers we'd need to rewrite a LOT of the game in order to do a proper fix, so this will have to do.
+	do {
 		currentTicks = (timeGetTime()+timeInfo.tickModifier) - timeInfo.firstTicks;
-	}
-	unsigned long intervalTicks = currentTicks - timeInfo.tickCount;
-
+	} while (currentTicks == timeInfo.tickCount);
+	
 	// limit maximum clock increment
+	unsigned long intervalTicks = currentTicks - timeInfo.tickCount;
 	if (intervalTicks > timeInfo.maxTickIncrement)
 	{
 		intervalTicks -= timeInfo.maxTickIncrement;
@@ -71,6 +71,6 @@ void UpdateTiming(void)
 	
 	timeInfo.speed = intervalTicks * mult;
 	timeInfo.frameCount = (unsigned long)(currentTicks * mult);
-	timeInfo.tickCount = currentTicks;	
+	timeInfo.tickCount = currentTicks;
 }
 
